c2507d11fdfeeb79d9193336c2461e0e94ba58ae
Troy <troyleak@users.noreply.github.com>
2015-10-02 15:07:07 -0700

    Initial commit
    Script depends on BeautifulSoup and Paperclip! Don’t forget to install
    them before running it!

---
+++ AHKScraper.py
@@ -0,0 +1,19 @@
+﻿from bs4 import BeautifulSoup as Soup
+import random
+import pyperclip
+
+random.seed()
+
+sourceFile = 'RandomWords.html'
+openFile = open(sourceFile)
+
+soup = Soup(openFile, 'html.parser')
+
+randList = []
+
+for li in soup.findAll('li'):
+    randList += li
+
+result = random.choice(randList)
+pyperclip.copy(result)
+print("added %s to clipboard" % result)
